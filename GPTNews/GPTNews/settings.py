65698251218e6ba2d0d579f39125ebe4d03003a2
RadiATsiyA <akkaunt.lewshi@list.ru>
2023-05-04 23:13:58 +0300

    django modifications

--- GPTNews/GPTNews/settings.py
+++ GPTNews/GPTNews/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+^a3dnytplp28+sv)+!vi)u@9djxfton*yjfqsrse8h*pues!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,3 +133,9 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- GPTNews/general/models.py
+++ GPTNews/general/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    name = models.CharField(max_length=300)
+    text = models.TextField(max_length=3500)
+    date = models.DateField(auto_now_add=True)
+    img_big = models.ImageField(upload_to='article_images_big')
+    img_small = models.ImageField(upload_to='article_images_small')
