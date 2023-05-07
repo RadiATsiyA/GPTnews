a15e8dbc0b23c7367ab848972eb0fb0c5af75eac
RadiATsiyA <akkaunt.lewshi@list.ru>
2023-05-08 00:13:35 +0300

    added models, migrated them, modified view file

--- GPTNews/general/models.py
+++ GPTNews/general/models.py
@@ -7,3 +7,23 @@ class Article(models.Model):
     date = models.DateField(auto_now_add=True)
     img_big = models.ImageField(upload_to='article_images_big')
     img_small = models.ImageField(upload_to='article_images_small')
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
+
+
+class Emails(models.Model):
+    email = models.EmailField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+
+class Suggestions(models.Model):
+    theme = models.CharField(max_length=300, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    used = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.id} {self.theme}'
--- GPTNews/general/views.py
+++ GPTNews/general/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
+import openai
+
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from common.views import TitleMixin
 
+from general.models import Article
 
-class IndexView(TitleMixin, TemplateView):
+
+class IndexView(TitleMixin, ListView):
+    model = Article
     template_name = 'general/index.html'
     title = 'Home'
+    paginate_by = 6
 
 
-class ArticleView(TitleMixin, TemplateView):
+class ArticleView(TitleMixin, DetailView):
+    model = Article
     template_name = 'general/article.html'
+    context_object_name = 'article'
     title = 'Article'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['article'] = Article.objects.filter(id=id)
 
 
 class LinksView(TitleMixin, TemplateView):
@@ -25,6 +39,28 @@ class HelloView(TitleMixin, TemplateView):
     title = 'Hell-o'
 
 
-class CreateNews(TitleMixin, TemplateView):
-    title = 'Create New!'
-    template_name = 'general/createnews.html'
+class CreateNews(TitleMixin, View):
+    def get(self, request):
+        return render(request, 'general/createnews.html')
+
+    def post(self, request):
+        context = {}
+        model_engine = 'gpt-3.5-turbo-0301' #'text-davinci-003'
+        max_tokens = 500
+
+        if request.method == 'POST':
+            theme = request.POST['theme']
+            promt = f'Generate fantastic news, and write an article for 300 words on topic {theme}'
+
+            completion = openai.Completion.create(engine=model_engine, promt=promt, max_tokets=max_tokens)
+            chatResponse = completion.choices[0].text
+
+            context = {
+                'chatResponse': chatResponse,
+                'promt': promt
+            }
+            return render(request, 'general/createnews.html', context)
+
+        else:
+            context["raise_error"] = "ERROR Occured or something went wrong."
+        return redirect('create_news')
