46a633eeeb3aa422417c62e4427b4eae17999288
RadiATsiyA <akkaunt.lewshi@list.ru>
2023-05-12 20:27:44 +0300

    Final

--- GPTNews/general/views.py
+++ GPTNews/general/views.py
@@ -1,12 +1,9 @@
-import openai
-
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from django.conf import settings
 from django.core.cache import cache
 
 from django.http import HttpResponseRedirect
@@ -14,11 +11,9 @@ from django.urls import reverse_lazy
 
 from common.views import TitleMixin
 
-from general.models import Article, Emails
-
+from general.models import Article, Emails, Suggestions
+from general.tasks import send_prompt_to_chatgpt_and_craiyon_and_save_obj_and_get_id
 from general.forms import EmailForm, SuggestionForm
-
-from craiyon import Craiyon
 
 
 class IndexView(TitleMixin, ListView):
@@ -41,7 +36,7 @@ class ArticleView(TitleMixin, DetailView):
             article = super().get_context_data()
             cache.set(cache_key, article, 43200)
         context = article
-        context['next_articles'] = Article.objects.order_by('date')[:6]
+        context['next_articles'] = Article.objects.all()[:6]
         context['form'] = EmailForm()
         return context
 
@@ -68,6 +63,8 @@ class HelloView(TitleMixin, TemplateView):
 
 
 class CreateNews(TitleMixin, View):
+    title = 'Create'
+
     def get(self, request, **kwargs):
         context = {
             'form': SuggestionForm()
@@ -75,27 +72,14 @@ class CreateNews(TitleMixin, View):
         return render(request, 'general/createnews.html', context)
 
     def post(self, request):
-        generator = Craiyon()
         if request.method == 'POST':
-            openai.api_key = settings.OPENAI_API_KEY
             form = SuggestionForm(request.POST)
-            theme = 'any topic'
             if form.is_valid():
                 theme = form.cleaned_data['theme']
+                article_id = send_prompt_to_chatgpt_and_craiyon_and_save_obj_and_get_id(theme)
+                suggestion_obj = Suggestions(theme=theme, used=True)
+                suggestion_obj.save()
+                return HttpResponseRedirect(reverse_lazy('article', kwargs={'pk': article_id}))
             else:
                 print(form.errors)
-            prompt = f'Generate a fantastic news and write an article for 230 words, on topic "{theme}"'
-
-            response = openai.Completion.create(
-                engine='text-davinci-003',
-                prompt=prompt,
-                max_tokens=300,
-                temperature=1
-            )
-            result = generator.generate(f"{theme}", negative_prompt="spoon", model_type="art")
-            url = result.images.pop(0)
-
-            article_obj = Article(name=theme, text=response['choices'][0]['text'], image_url=url)
-            article_obj.save()
-            return HttpResponseRedirect(reverse_lazy('article', kwargs={'pk': article_obj.id}))
         return render(request, 'general/createnews.html')
