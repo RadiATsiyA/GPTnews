3fef5351d799493e9d7c65567350626a08ca1ad9
RadiATsiyA <akkaunt.lewshi@list.ru>
2023-05-13 13:55:29 +0300

    final version

--- GPTNews/GPTNews/settings.py
+++ GPTNews/GPTNews/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+^a3dnytplp28+sv)+!vi)u@9djxfton*yjfqsrse8h*pues!!vi)u@9djxfton*yjfqsrse8h*pues!'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -149,7 +149,7 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 MEDIA_URL = '/media/'
